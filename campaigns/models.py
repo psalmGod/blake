1e8d22ff6fa1e3dbc70f226193d3c5d4db52ecfa
psalmGod <samsonokaome@gmail.com>
2025-01-17 07:11:16 -0800

    new

--- campaigns/models.py
+++ campaigns/models.py
@@ -5,10 +5,14 @@ from django.db import models
 
 from django.db import models
 
+from django.db import models
+from django.utils import timezone
+
 class EmailGroup(models.Model):
     name = models.CharField(max_length=255)
     email = models.EmailField()
-    first_name = models.CharField(max_length=255, null=True, blank=True)  # Add this line
+    first_name = models.CharField(max_length=255, null=True, blank=True)
+    created_at = models.DateTimeField(default=timezone.now)  # Add this line
 
     def __str__(self):
         return self.name
